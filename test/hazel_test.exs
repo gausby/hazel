18d99c8c4c8a41e8d80f62e28de2d863ada22b60
Martin Gausby <martin@gausby.dk>
2016-08-14 12:35:22 +0200

    Code reformatting

--- test/hazel_test.exs
+++ test/hazel_test.exs
@@ -7,18 +7,17 @@ defmodule HazelTest do
       target = 100
       assert ^target =
         Stream.repeatedly(&Hazel.generate_peer_id/0)
-        |> Enum.take(target)
-        |> Enum.uniq
+        |> Stream.take(target)
+        |> Stream.uniq
+        |> Enum.to_list
         |> length
     end
 
     test "peer_ids should be 20 bytes long" do
-      target = 100
-      assert ^target =
-        Stream.repeatedly(&Hazel.generate_peer_id/0)
-        |> Enum.take(target)
-        |> Enum.filter(fn peer_id -> byte_size(peer_id) == 20 end)
-        |> length
+      Stream.repeatedly(&Hazel.generate_peer_id/0)
+      |> Stream.take(100)
+      |> Stream.filter(fn peer_id -> assert byte_size(peer_id) == 20 end)
+      |> Stream.run()
     end
   end
 end
